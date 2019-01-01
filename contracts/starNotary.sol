c7d4767b00be9c148ff00c4c96730a638c77ebf5
Wang Qunfeng <qunfengw@gmail.com>
2019-01-01 12:13:04 +0800

    Update starNotary.sol

--- contracts/starNotary.sol
+++ contracts/starNotary.sol
@@ -25,7 +25,6 @@ contract StarNotary is ERC721 {
     }
 
     function lookUptokenIdToStarInfo(uint256 _tokenId) public view returns (string) {
-        //make sure star exists
         require(bytes(tokenIdToStarInfo[_tokenId].name).length > 0);
         return tokenIdToStarInfo[_tokenId].name;
     }
@@ -55,23 +54,19 @@ contract StarNotary is ERC721 {
       }
 
     function exchangeStars(uint256 _firstUserTokenId, uint256 _secondUserTokenId) public {
-        //make sure both stars exist
         require(bytes(tokenIdToStarInfo[_firstUserTokenId].name).length > 0);
         require(bytes(tokenIdToStarInfo[_secondUserTokenId].name).length > 0);
-        //make sure token ids are unique
         require(_firstUserTokenId != _secondUserTokenId, "Token Ids are the same");
-        //store in memory the first user's star
+        
         Star memory tempStar = tokenIdToStarInfo[_firstUserTokenId];
         tokenIdToStarInfo[_firstUserTokenId] = tokenIdToStarInfo[_secondUserTokenId];
         tokenIdToStarInfo[_secondUserTokenId] = tempStar;
     }
 
     function transferStar(address _transferAddress, uint256 _tokenId) public {
-        //make sure star exists
         require(bytes(tokenIdToStarInfo[_tokenId].name).length > 0);
-        //make sure star is not getting transferred to self
         require(_transferAddress != msg.sender);
-        //make transfer
+        
         _removeTokenFrom(msg.sender, _tokenId);
         _addTokenTo(_transferAddress, _tokenId);
     }
